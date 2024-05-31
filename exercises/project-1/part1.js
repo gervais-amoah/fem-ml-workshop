4b960317f5543fa7e791b114d052551929b746c3
gervais <gervaisamoah@yahoo.com>
2024-05-31 09:58:22 +0000

    simple image detection

---
+++ exercises/project-1/part1.js
@@ -0,0 +1,20 @@
+import "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import { handleFilePicker, showResult } from "./utils";
+
+let model;
+
+const init = async () => {
+  model = await cocossd.load();
+
+  handleFilePicker(predict);
+};
+
+const predict = async (img) => {
+  console.log("ok - start");
+  const predictions = await model.detect(img);
+  console.log(predictions);
+  showResult(predictions);
+};
+
+init();
