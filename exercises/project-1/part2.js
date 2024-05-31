3e68bc49a424e0eb606aa3f35d0648b032acc2b8
gervais <gervaisamoah@yahoo.com>
2024-05-31 10:23:39 +0000

    Object detection with webcam

---
+++ exercises/project-1/part2.js
@@ -0,0 +1,23 @@
+import "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import { showResult, startWebcam, takePicture } from "./utils";
+
+const webcamButton = document.getElementById("webcam");
+const captureButton = document.getElementById("pause");
+const video = document.querySelector("video");
+
+let model;
+
+const init = async () => {
+  model = await cocossd.load();
+};
+
+webcamButton.onclick = () => startWebcam(video);
+captureButton.onclick = () => takePicture(video, predict);
+
+const predict = async (img) => {
+  const predictions = await model.detect(img);
+  showResult(predictions);
+};
+
+init();
